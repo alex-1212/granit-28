a9d4643328a864673417756cb41592f78bb29c45
alex-1212 <105470358+alex-1212@users.noreply.github.com>
2025-03-31 21:42:14 +1000

    Update AboutSection.tsx

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -59,7 +59,7 @@ const AboutSection: React.FC = () => {
           <div className="relative">
             <div className="absolute inset-0 bg-gradient-to-r from-primary/5 to-primary/10 dark:from-primary/10 dark:to-primary/20 rounded-2xl animate-on-scroll"></div>
             <div className="glass-card rounded-2xl overflow-hidden aspect-video relative animate-on-scroll">
-              <img src="https://avmining.ru/wp-content/uploads/2023/09/4.jpg" alt="О компании ООО Гранит" className="object-cover w-full h-full" />
+              <img src="https://granit-svg.ru/img-granit/expert1.webp" alt="О компании ООО Гранит" className="object-cover w-full h-full" />
             </div>
             
             <div className="absolute -bottom-8 -left-8 glass-card-accent rounded-xl p-4 max-w-[250px] animate-on-scroll">
